2f6491bb9ce2d09da826238aa55bbd5cb86dac48
Pforsich <Lindsay.M.Pforsich@disney.com>
2018-01-27 21:24:25 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -10,7 +10,29 @@ console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
 */
 
-var cards = ["queen", "king", "king", "queen"];
+var cards = [
+  {
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+  },
+  {
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+  },
+  {
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+  }
+];
+//var cards = ["queen", "king", "king", "queen"];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -33,9 +55,10 @@ var flipCard = function(cardId){
   cardsInPlay.push(cardTwo);
   console.log("User flipped " + cardsInPlay[1]);
 */
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
-
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 
   if(cardsInPlay.length === 2) {
       checkForMatch();
