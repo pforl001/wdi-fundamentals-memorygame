922bfad02b532215d4ec29c4492bbce1a3abb249
Pforsich <Lindsay.M.Pforsich@disney.com>
2018-01-28 20:51:26 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,6 @@
 console.log("Up and running!");
 
-/*
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-*/
-
+// -cards- is an array of 4 card objects, which include the properties: rank, suit, and cardImage
 var cards = [
   {
   rank: "queen",
@@ -32,9 +23,13 @@ var cards = [
   cardImage: "images/king-of-diamonds.png"
   }
 ];
-//var cards = ["queen", "king", "king", "queen"];
+
+// -cardsInPlay- is an empty array
+// when a user flips a card, that card's rank will be pushed to this array
 var cardsInPlay = [];
 
+// -checkForMatch- is a function which checks the equivalence of the first 2 card ranks in the cardsInPlay array
+// It provides an alert to tell the user if it's a match or if they should try again
 var checkForMatch = function() {
   if(cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
@@ -43,33 +38,29 @@ var checkForMatch = function() {
   }
 };
 
-var flipCard = function(){
-  // Function that stores all steps that should happen when user flips a card
-  // "cardId" parameter refers to the array index number passed in
-  // removing cardId parameter and creating a variable below to dynamically grab the cardId of the card that the user chooses to click on; no longer "hard-coded"
-  var cardId = this.getAttribute('data-id'); // "this" refers to what card was just clicked on, from the Event Listener in createBoard() below
+// -flipCard- is a function that stores all the steps that will happen when a user flips a card
+// There is no cardId parameter; rather, the cardId variable will dynamically get the id of the flipped card, rather than being hard-coded
+var flipCard = function() {
+
+  var cardId = this.getAttribute('data-id'); // "this" refers to the card that was just clicked on, from the Event Listener in createBoard() below
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
 
+  // when the user flips a card, set it to the corresponding cardImage, replacing the back image
   this.setAttribute('src', cards[cardId].cardImage);
 
   if(cardsInPlay.length === 2) {
       checkForMatch();
   }
-  /*Deleting following block of code:
-    var cardOne = cards[0];
-    cardsInPlay.push(cardOne);
-    console.log("User flipped " + cardsInPlay[0]);
-    var cardTwo = cards[2];
-    cardsInPlay.push(cardTwo);
-    console.log("User flipped " + cardsInPlay[1]);
-  */
 };
 
+// -createBoard- function creates a brand new game board
+// loop through each object in the cards array, and set them up with the back image
+// wait/listen for the user to click on a card and when they do, call the flipCard function
 var createBoard = function() {
-  for(var i = 0; i < cards.length; i++) { // does this look through cards (array of objects) or cardsInPlay?
+  for(var i = 0; i < cards.length; i++) {
     var cardElement = document.createElement('img');
     cardElement.setAttribute('src', 'images/back.png');
     cardElement.setAttribute('data-id', i);
@@ -79,6 +70,5 @@ var createBoard = function() {
   }
 };
 
+  // call createBoard() function to kick off the game!
   createBoard();
-  //flipCard(0);
-  //flipCard(2);
